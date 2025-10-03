2bedf73602d70f6430b1d232968f757529ddfe4e
Miggie <miguelt613377@gmail.com>
2025-10-03 19:13:42 -0400

    Add DialogueBox feature and new assets
    Introduced DialogueBox.cs script and prefab for dialogue functionality. Added player headshot art, Chiron GoRound TC font assets, and updated input actions and test dialogue scene to support new dialogue system.

---
+++ Pumpkin Pie/Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class DialogueBox : MonoBehaviour
+{
+    [SerializeField] private InputActionReference interact;
+
+    [SerializeField] private Image headshot;
+    [SerializeField] private TextMeshProUGUI text;
+
+    [SerializeField] private List<Image> headshots;
+    [SerializeField] private List<string> texts;
+    private int pointer;
+    private int maxPointer;
+
+    void Start()
+    {
+        pointer = 0;
+        maxPointer = texts.Count;
+    }
+
+    private void OnEnable()
+    {
+        interact.action.Enable();
+        interact.action.performed += OnInteract;
+    }
+
+    private void OnDisable()
+    {
+        interact.action.performed -= OnInteract;
+        interact.action.Disable();
+    }
+
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        pointer++;
+    }
+
+    void Update()
+    {
+        if (pointer < maxPointer)
+        {
+            headshot = headshots[pointer];
+            text.text = texts[pointer];
+        }
+    }
+}
